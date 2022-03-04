d7dd4a5ab82a4a4470a89dd301abe912570063ac
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-03-04 15:31:34 +0100

    movies data comes in home view from db

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
+
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index () {
-        return view('home');
-      }
+    public function index()
+    {
+
+        $movies = Movie::all();
+
+
+        if (empty($movies)) {
+            abort("404");
+        }
+        dump($movies);
+
+        return view('home', compact('movies'));
+    }
 }
